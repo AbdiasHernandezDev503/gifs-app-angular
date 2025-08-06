d0bb4afcae5d31aa22b49544849530557cbfa2c7
Abdías Hernández <abdiashdz034@outlook.com>
2025-08-05 18:34:43 -0600

    Creando el servicio que hará consumo de la API de Giphy y mostrando resultados en la vista de Trending y Busquedas

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '@environments/environment';
+import type { GiphyResponse } from '../interfaces/giphy.interfaces';
+import { Gif } from '../interfaces/gif.interface';
+import { GifMapper } from '../mapper/gif.mapper';
+import { map } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class GifsService {
+  private http = inject(HttpClient);
+  trendingGifs = signal<Gif[]>([]);
+  trendingGifsLoading = signal(true);
+
+  constructor() {
+    this.loadTrendingGifs();
+  }
+
+  loadTrendingGifs() {
+    this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
+      params: {
+        api_key: environment.giphyApiKey,
+        limit:25
+      }
+    }).subscribe( (resp) => {
+      const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+      this.trendingGifs.set(gifs);
+      this.trendingGifsLoading.set(false);
+    } );
+  }
+
+  searchGifs(query : string) {
+    return this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+      params : {
+        api_key: `${environment.giphyApiKey}`,
+        q: query,
+        limit: 25
+      }
+    }).pipe(
+      map(({data}) => data),
+      map((items) => GifMapper.mapGiphyItemsToGifArray(items))
+
+      // TODO: historial
+    );
+    /*.subscribe( (resp) => {
+      const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+      console.log(gifs);
+    })*/
+  }
+
+}
